6ce70a3d9fd6c3740bf753067ce80b73fb9a5ab5
jothbc <jothbc@gmail.com>
2020-03-28 22:29:22 -0300

    Testes BackEnd

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const cors = require('cors');
-
+const {errors} = require('celebrate')
 const routes = require('./routes');
 
 
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
+module.exports = app;
 
-app.listen(3333);
 
